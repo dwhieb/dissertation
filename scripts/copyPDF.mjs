2985466fd22552a38b65b8b394360e88bb6ef0ac
Danny Hieber <dwhieb@gmail.com>
2019-06-30 12:39:56 -0500

    copyPDF.mjs

---
+++ scripts/copyPDF.mjs
@@ -0,0 +1,34 @@
+import { createRequire } from 'module';
+import createSpinner     from 'ora';
+import fs                from 'fs';
+import path              from 'path';
+import rootPath          from './rootPath.mjs';
+
+const { copyFile: copy } = fs.promises;
+const require            = createRequire(import.meta.url);
+
+const srcPath  = path.join(rootPath, `src/main.pdf`);
+const destPath = path.join(rootPath, `dissertation.pdf`);
+
+async function copyPDF() {
+
+  const spinner = createSpinner(`Copying PDF`).start();
+
+  try {
+
+    await copy(srcPath, destPath);
+
+  } catch (e) {
+
+    if (e.code === `ENOENT`) return spinner.warn(`PDF does not exist`);
+    spinner.fail(e.message);
+
+  }
+
+  spinner.succeed(`PDF copied`);
+
+}
+
+if (require.main === undefined) copyPDF();
+
+export default copyPDF;
---
+++ scripts/upload.mjs
@@ -0,0 +1,12 @@
+import Azure from '@azure/storage-blob';
+
+const require = createRequire(import.meta.url);
+
+async function upload() {
+
+
+}
+
+if (require.main === undefined) upload();
+
+export default upload;
