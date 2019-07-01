f670d20f7a734444defe87b06e30b8afd96819e1
Danny Hieber <dwhieb@gmail.com>
2019-06-30 20:30:11 -0500

    create versioned instances of PDF

--- scripts/build/copyFiles.mjs
+++ scripts/build/copyFiles.mjs
@@ -5,23 +5,27 @@
 import { createRequire } from 'module';
 import createSpinner     from 'ora';
 import fs                from 'fs';
+import meta              from '../../package.json';
 import path              from 'path';
 import rootDir           from '../constants/rootDir.mjs';
 
 const { copyFile: copy } = fs.promises;
 const require            = createRequire(import.meta.url);
 
-const srcPath  = path.join(rootDir, `src/main.pdf`);
-const destPath = path.join(rootDir, `dissertation.pdf`);
+const distFilePath = path.join(rootDir, `src/main.pdf`);
 
-async function copyPDF() {
+async function copyFiles() {
 
-  const spinner = createSpinner(`Copying PDF`);
+  const spinner = createSpinner(`Copying PDF files`);
   spinner.start();
 
   try {
 
-    await copy(srcPath, destPath);
+    // Copy latest dist to unversioned PDF
+    await copy(distFilePath, path.join(rootDir, `dissertation.pdf`));
+
+    // Copy latest dist to versioned PDF
+    await copy(distFilePath, path.join(rootDir, `archive/v${meta.version}.pdf`));
 
   } catch (e) {
 
@@ -34,6 +38,6 @@ async function copyPDF() {
 
 }
 
-if (require.main === undefined) copyPDF();
+if (require.main === undefined) copyFiles();
 
-export default copyPDF;
+export default copyFiles;
