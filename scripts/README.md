4c565442b2c0ed16cd1f3e6c43d31270164e3a7d
Danny Hieber <dwhieb@gmail.com>
2019-08-04 11:35:29 -0500

    write script to rename SBC files

---
+++ scripts/README.md
@@ -0,0 +1,3 @@
+# Scripts
+
+This folder contains scripts used in creating the LaTeX document that this dissertation was produced from.
---
+++ scripts/temp.js
@@ -0,0 +1,30 @@
+/* eslint-disable
+  guard-for-in,
+  no-await-in-loop,
+*/
+
+import fs   from 'fs';
+import path from 'path';
+
+const {
+  readdir: readDir,
+  rename,
+} = fs.promises;
+
+const textsDir = `data/English/SBC/texts`;
+
+void async function renameFiles() {
+
+  const filenames = await readDir(textsDir);
+
+  for (const oldFilename in filenames) {
+
+    const newFilename = oldFilename
+    .replace(/^sbc/u, `SBC`)
+    .replace(/_dt2\.txt$/u);
+
+    await rename(path.join(textsDir, oldFilename), path.join(textsDir, newFilename));
+
+  }
+
+}();
