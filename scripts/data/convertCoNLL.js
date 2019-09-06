188002a0b446b369150f584a2f3b62c70fde3e1a
Daniel W. Hieber <dwhieb@gmail.com>
2019-09-05 21:46:30 -0500

    Data: clean up CoNLL > JSON script

--- scripts/data/convertCoNLL.js
+++ scripts/data/convertCoNLL.js
@@ -1,6 +1,8 @@
 import createParser from 'csv-parse';
 import fs           from 'fs';
 import path         from 'path';
+import ProgressBar  from 'progress';
+import recurse      from 'recursive-readdir';
 
 const columns = [
   `ID`,
@@ -20,11 +22,13 @@ const parserOptions = {
   trim:               true,
 };
 
+const [,, dataDir] = process.argv;
+
 /**
  * Converts a CoNLL CSV file to JSON and saves it alongside the original
  * @param  {String} filePath The path to the CoNLL file to convert
  */
-export default function convertCoNLL(filePath) {
+const convertCoNLL = filePath => new Promise((resolve, reject) => {
 
   const dir          = path.dirname(filePath);
   const filename     = path.basename(filePath, `.conll`);
@@ -38,7 +42,7 @@ export default function convertCoNLL(filePath) {
 
   writeStream.write(`[\n`);
 
-  parser.on(`error`, console.error);
+  parser.on(`error`, reject);
 
   parser.on(`readable`, () => {
 
@@ -64,8 +68,40 @@ export default function convertCoNLL(filePath) {
 
   });
 
-  parser.on(`end`, () => writeStream.write(`]`));
+  parser.on(`end`, () => writeStream.end(`]`, resolve));
 
   readStream.pipe(parser);
 
+});
+
+/**
+ * The ignore function for the recurse method
+ */
+function ignore(filePath, stats) {
+  if (stats.isDirectory()) return false;
+  return path.extname(filePath) === `.json`;
 }
+
+/**
+ * Converts the CoNLL version of the MASC data to JSON
+ * @return {Promise}
+ */
+void async function convert() {
+
+  try {
+
+    const files       = await recurse(dataDir, [ignore]);
+    const progressBar = new ProgressBar(`:bar`, { total: files.length });
+
+    for (const filepath of files) {
+      await convertCoNLL(filepath); // eslint-disable-line no-await-in-loop
+      progressBar.tick();
+    }
+
+  } catch (e) {
+
+    console.error(e);
+
+  }
+
+}();
