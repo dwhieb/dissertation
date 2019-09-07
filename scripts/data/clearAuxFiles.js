a312f6b5c6c8cbe2cc76b154d2df3b3947dd2ac0
Danny Hieber <dwhieb@gmail.com>
2019-09-07 16:44:35 -0500

    allow clearAuxFiles to be run from script or cmd line

--- scripts/data/clearAuxFiles.js
+++ scripts/data/clearAuxFiles.js
@@ -2,9 +2,13 @@
  * Removes all JSON files from the provided directory
  */
 
-import fs             from 'fs';
-import path           from 'path';
-import { processDir } from '../utilities/index.js';
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import fs                from 'fs';
+import path              from 'path';
+import { processDir }    from '../utilities/index.js';
+
+const require = createRequire(fileURLToPath(import.meta.url)); // eslint-disable-line no-shadow
 
 const { unlink } = fs.promises;
 
@@ -19,4 +23,6 @@ function ignore(filePath, stats) {
   return path.extname(filePath) !== `.json`;
 }
 
-processDir(dir, clearAuxFiles, ignore);
+if (require.main === module) processDir(dir, clearAuxFiles, ignore);
+
+export default () => processDir(dir, clearAuxFiles, ignore);
