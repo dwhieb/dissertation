3eb77f31a3bed83e783be4621cd97fa444410b88
Danny Hieber <dwhieb@gmail.com>
2019-09-07 16:30:45 -0500

    remove aux files

--- scripts/stats/index.js
+++ scripts/stats/index.js
@@ -1,11 +1,38 @@
-import fs             from 'fs';
-import JSONStream     from 'JSONStream';
-import path           from 'path';
-import { processDir } from '../utilities/index.js';
+// IMPORTS
 
+import csvStringify  from 'csv-stringify';
+import fs            from 'fs';
+import JSONStream    from 'JSONStream';
+import path          from 'path';
+import { promisify } from 'util';
+
+import {
+  compare,
+  processDir,
+} from '../utilities/index.js';
+
+const json2csv      = promisify(csvStringify);
 const { writeFile } = fs.promises;
 
+// VARIABLES
+
+const columns = [
+  `wordform`,
+  `frequency`,
+];
+
+const csvOptions = {
+  columns,
+  delimiter: `\t`,
+  header:    true,
+};
+
 const [,, dataDir] = process.argv;
+
+// METHODS
+
+const convertFrequencies = map => [...map.entries()]
+.sort(([wordA, freqA], [wordB, freqB]) => compare(freqB, freqA) || compare(wordA, wordB));
 
 const generateWordforms = filePath => new Promise((resolve, reject) => {
 
@@ -17,10 +44,22 @@ const generateWordforms = filePath => new Promise((resolve, reject) => {
   const parser     = JSONStream.parse(`*.token`);
   const readStream = fs.createReadStream(filePath);
 
+  const frequencies = new Map;
+
   parser.on(`error`, reject);
 
+  parser.on(`data`, token => {
+
+    token = token.toLowerCase(); // eslint-disable-line no-param-reassign
+
+    if (frequencies.has(token)) frequencies.set(token, frequencies.get(token) + 1);
+    else frequencies.set(token, 1);
+
+  });
+
   parser.on(`end`, async () => {
-    await writeFile(newFilePath, ``, `utf8`);
+    console.log(await json2csv(convertFrequencies(frequencies), csvOptions));
+    // await writeFile(newFilePath, ``, `utf8`);
     resolve();
   });
 
@@ -33,5 +72,7 @@ function ignore(filePath, stats) {
   if (filePath.endsWith(`_wordforms.json`)) return true;
   return path.extname(filePath) !== `.json`;
 }
+
+// MAIN
 
 processDir(dataDir, generateWordforms, ignore);
