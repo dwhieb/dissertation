397280c87713a9bb2ea429ecc3de24bb96e4869e
Danny Hieber <dwhieb@gmail.com>
2019-09-07 16:54:17 -0500

    regenerate aux files

--- scripts/stats/index.js
+++ scripts/stats/index.js
@@ -1,6 +1,7 @@
 import clearAuxFiles     from '../data/clearAuxFiles.js';
 import convertCoNLL      from '../data/convertCoNLL.js';
 import { fileURLToPath } from 'url';
+import generateWordforms from './generateWordforms.js';
 import path              from 'path';
 import removeBadTokens   from '../data/removeBadTokens.js';
 
@@ -10,7 +11,21 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const dataDir = path.join(__dirname, `../../data/English/data`);
 
 void async function stats() {
+
+  // Data Preparation
+
+  console.info(`removing auxiliary files`);
   await clearAuxFiles(dataDir);
+
+  console.info(`converting CoNLL files to JSON`);
   await convertCoNLL(dataDir);
+
+  console.info(`removing unwanted data`);
   await removeBadTokens(dataDir);
+
+  // Statistics
+
+  console.info(`generating wordforms`);
+  await generateWordforms(dataDir);
+
 }();
