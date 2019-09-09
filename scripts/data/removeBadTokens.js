38c859b1e069de431641ebb27963c231537004a9
Danny Hieber <dwhieb@gmail.com>
2019-09-09 11:04:58 -0500

    add more chars to bad chars list

--- scripts/data/removeBadTokens.js
+++ scripts/data/removeBadTokens.js
@@ -7,7 +7,7 @@ import { processDir } from '../utilities/index.js';
 
 const { rename, unlink } = fs.promises;
 
-const badCharsRegExp = /[-_./%�[\]0-9]/gu;
+const badCharsRegExp = /[-_./%*+&|?@\\~#�><[\]0-9]/gu;
 const pos            = Object.keys(pennTags);
 
 /**
