ed74d363b0ecf90a5a8f7749ac29c6ea671e6dcb
Daniel W. Hieber <dwhieb@gmail.com>
2019-09-16 21:46:27 -0500

    Data: delete extraneous scripts (#669)
    closes #669

--- scripts/ANC/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eu
-
-DIR=`dirname ./deploy.sh`
-java -Xmx800M -jar $DIR/ANCTool-3.0.2.jar
--- scripts/utilities/index.js
+++ scripts/utilities/index.js
@@ -1,13 +1,7 @@
-import aggregate          from './aggregate.js';
-import compare            from './compare.js';
-import convertFrequencies from './convertFrequencies.js';
-import isMajorCategory    from './isMajorCategory.js';
-import processDir         from './processDir.js';
+import compare    from './compare.js';
+import processDir from './processDir.js';
 
 export {
-  aggregate,
   compare,
-  convertFrequencies,
-  isMajorCategory,
   processDir,
 };
