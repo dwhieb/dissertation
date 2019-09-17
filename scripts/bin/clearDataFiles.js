c153b49cbd09764c96dc29eeb6ba472b2b74e76b
Danny Hieber <dwhieb@gmail.com>
2019-09-16 21:40:35 -0500

    remove bin scripts

--- scripts/bin/clearDataFiles.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import clearDataFiles from '../data/clearDataFiles.js';
-
-const [,, dir] = process.argv;
-
-if (!dir) throw new Error(`Please provide the path to the directory you wish to clear`);
-
-clearDataFiles(dir);
--- scripts/bin/removeUnwantedTokens.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import removeUnwantedTokens from '../data/removeUnwantedTokens.js';
-
-const [,, dir] = process.argv;
-
-if (!dir) throw new Error(`Please provide the path to the directory of JSON files you wish to remove unwanted data from`);
-
-removeUnwantedTokens(dir);
