7550989f6962b0bb0b17e13d6479935d749ca562
Danny Hieber <dwhieb@gmail.com>
2019-09-25 12:31:20 -0500

    restart

--- scripts/bin/convertOANC.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import convertOANC from '../data/convertOANC.js';
-
-const [,, dataDir] = process.argv;
-
-if (!dataDir) throw new Error(`Please provide the path to the directory of text files you wish to convert.`);
-
-convertOANC(dataDir);
