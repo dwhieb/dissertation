51fce3aae58e7f97ac4f71fcd186d5ca33156810
Danny Hieber <dwhieb@gmail.com>
2019-09-25 18:07:10 -0500

    English: visualize wordform frequencies
    closes #699

---
+++ data/English/figures/wordform_frequencies/wordform_frequencies.R
@@ -0,0 +1,28 @@
+wordforms_path <- 'data/English/stats/wordforms.tsv'
+graph_path     <- 'data/English/figures/wordform_frequencies.pdf'
+
+# load data
+data <- read.table(
+  file_path,
+  colClasses = c('character', 'integer'),
+  header     = TRUE,
+  quote      = '',
+  sep        = '\t',
+)
+
+# start PDF generation
+pdf(graph_path)
+
+# density plot
+d <- density(log(data$frequency))
+
+plot(
+  d,
+  main = 'Density of (log) Wordform Frequencies',
+)
+
+# end PDF generation
+dev.off()
+
+# show summary statistics
+print(summary(data$frequency))
