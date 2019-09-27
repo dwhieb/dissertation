f7274836d859820cee5e2448a42e6b98e1bcaefa
Danny Hieber <dwhieb@gmail.com>
2019-09-26 21:32:30 -0500

    add filter method to generateWordforms

--- scripts/bin/generateWordforms.js
+++ scripts/bin/generateWordforms.js
@@ -2,9 +2,9 @@
 
 import generateWordforms from '../stats/generateWordforms.js';
 
-const [,, dataDir, outputDir] = process.argv;
+const [,, dataDir, outputPath] = process.argv;
 
 if (!dataDir) throw new Error(`Please provide the path to the data directory as the first argument.`);
-if (!outputDir) throw new Error(`Please provide the path where you would like the wordforms file generated.`);
+if (!outputPath) throw new Error(`Please provide the path where you would like the wordforms file generated.`);
 
-generateWordforms(dataDir, outputDir).catch(console.error);
+generateWordforms(dataDir, outputPath).catch(console.error);
