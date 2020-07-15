995aea2805d68d863ba90281d21729bcbe77ee99
Danny Hieber <dwhieb@gmail.com>
2020-07-14 19:06:34 -0500

    test run

---
+++ data/Nuuchahnulth/scripts/lemmatize.js
@@ -0,0 +1,51 @@
+// if the gloss consists of nothing but uppercase letters, numbers, and periods
+// remove it from the array; the remaining morphemes constitute the stem;
+// reconstruct the stem from the morphemes by concatenating the array (via reduce?)
+
+/**
+ * This script lemmatizes each token in the corpus, assigning it to a stem.
+ */
+
+/* eslint-disable
+  max-len,
+  max-nested-callbacks,
+  no-param-reassign,
+  no-undefined,
+*/
+
+import findAndReplace from '../../../scripts/utilities/findAndReplace.js';
+
+findAndReplace(utterance => {
+
+  if (!utterance.words) return utterance;
+
+  utterance.words.forEach(word => {
+
+    if (!word?.morphemes?.length) return;
+
+    const grammaticalGlossRegExp = /^[A-Z123.]+$/u;
+
+    const morphemeTypes = word.morphemes
+    .map(morpheme => (grammaticalGlossRegExp.test(morpheme.gloss) ? null : morpheme.gloss));
+
+    // start at index 1 so as to skip any reduplication morphemes at the start of the word
+    let firstGrammaticalMorphemeIndex = morphemeTypes.indexOf(null, 1);
+    firstGrammaticalMorphemeIndex     = firstGrammaticalMorphemeIndex === -1 ? undefined : firstGrammaticalMorphemeIndex;
+
+    const lexicalMorphemes = word.morphemes
+    .slice(0, firstGrammaticalMorphemeIndex);
+
+    if (morphemeTypes[0] === null) lexicalMorphemes.shift();
+
+    word.stem = lexicalMorphemes
+    .map(morpheme => morpheme.transcription)
+    .join(`-`);
+
+  });
+
+  return utterance;
+
+}, { searchOnly: false, testRun: true })
+.catch(e => {
+  throw e;
+});
