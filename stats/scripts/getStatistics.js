e1b70b8904c273549e019f936f053e260a264331
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-06 19:24:48 -0500

    move compare.js

--- stats/scripts/getStatistics.js
+++ stats/scripts/getStatistics.js
@@ -12,6 +12,7 @@
   no-param-reassign,
 */
 
+import compare       from '../stats/scripts/utilities/compare.js';
 import csvStringify  from 'csv-stringify';
 import fs            from 'fs-extra';
 import path          from 'path';
@@ -25,20 +26,6 @@ const {
 } = fs;
 
 const json2csv = promisify(csvStringify);
-
-/**
- * A generic comparator function, for use with the sort method. Works with Strings and Numbers.
- * @param  {Any}     a First item to be compared
- * @param  {Any}     b Second item to be compared
- * @return {Integer}   Returns 1 if the first item is greater, -1 if the first item is lesser, and 0 if the two items are the same.
- * @example
- * items.sort((a, b) => compare(a.year, b.year) || compare(a.title, b.title));
- */
-function compare(a, b) {
-  if (a < b) return -1;
-  if (a > b) return +1;
-  return 0;
-}
 
 /**
  * Ignore method which tells recursive-readdir to ignore any non-JSON files
