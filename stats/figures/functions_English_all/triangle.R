bbe5ca500d376f594814efb1cd86b6cf3af049aa
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-08 12:46:06 -0500

    English discourse functions (all) - triangle

---
+++ stats/figures/functions_English_all/triangle.R
@@ -0,0 +1,23 @@
+library(ggtern)
+source("stats/scripts/load_data.R")
+
+language <- "English"
+
+file_path  <- paste("stats/data/English_archlexemes_all.tsv", sep = "")
+
+data <- load_data(file_path)
+
+functions <- 1 - data[7:9] # DP
+
+plot <- ggtern(functions, aes(dispersionREF, dispersionPRED, dispersionMOD)) +
+  Tlab("Predication") +
+  Llab("Reference") +
+  Rlab("Modification") +
+  tern_limits(T = 1.05, L = 1.05, R = 1.05) +
+  geom_point()
+
+ggsave(
+  "stats/figures/functions_English_all/triangle.png",
+  width  = 10,
+  height = 10
+)
