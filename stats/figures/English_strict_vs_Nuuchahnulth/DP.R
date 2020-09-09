0a90b4758f4767788af61dcf9bc39f4747c0fde5
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-09 14:59:33 -0500

    English (strict) vs. Nuuchahnulth

--- stats/figures/English_strict_vs_Nuuchahnulth/DP.R
+++ stats/figures/English_strict_vs_Nuuchahnulth/DP.R
@@ -11,7 +11,7 @@ functions_English <- 1 - data.frame(
   data_English$dispersion_mod
 )
 
-plot_English <- plot_triangle(functions_English, "English (strict)")
+plot_English <- plot_triangle(functions_English, "English (strict)", "Deviation of Proportions (DP)")
 
 file_path_Nuuchahnulth <- "stats/data/Nuuchahnulth_archlexemes.tsv"
 data_Nuuchahnulth      <- load_data(file_path_Nuuchahnulth)
@@ -22,12 +22,12 @@ functions_Nuuchahnulth <- 1 - data.frame(
   data_Nuuchahnulth$dispersion_mod
 )
 
-plot_Nuuchahnulth <- plot_triangle(functions_Nuuchahnulth, "Nuuchahnulth")
+plot_Nuuchahnulth <- plot_triangle(functions_Nuuchahnulth, "Nuuchahnulth", "Deviation of Proportions (DP)")
 
 plots <- ggtern::grid.arrange(plot_English, plot_Nuuchahnulth, ncol = 2)
 
 ggsave(
-  "stats/figures/English_strict_vs_Nuuchahnulth/comparison.png",
+  "stats/figures/English_strict_vs_Nuuchahnulth/DP.png",
   plots,
   height = 10,
   width = 10,
