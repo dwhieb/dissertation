fb2f4ad2af079f49bf6cbc250e009b9cd421dd60
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-09 15:35:56 -0500

    English words

--- stats/figures/English_words/English_words.R
+++ stats/figures/English_words/English_words.R
@@ -5,33 +5,70 @@ source("stats/scripts/name_cols.R")
 
 file_path <- "stats/data/English_archlexemes.tsv"
 data      <- load_data(file_path)
-cols      <- c("ref", "pred", "mod")
 
-DP_broad <- 1 - data.frame(
+rel_freq_broad <- name_cols(data.frame(
+  data$ref_rel_broad,
+  data$pred_rel_broad,
+  data$mod_rel
+))
+
+rel_freq_strict <- name_cols(data.frame(
+  data$ref_rel,
+  data$pred_rel,
+  data$mod_rel
+))
+
+DP_broad <- 1 - name_cols(data.frame(
   data$dispersion_ref_broad,
   data$dispersion_pred_broad,
   data$dispersion_mod
-)
+))
 
-DP_strict <- 1 - data.frame(
+DP_strict <- 1 - name_cols(data.frame(
   data$dispersion_ref,
   data$dispersion_pred,
   data$dispersion_mod
-)
-
-name_cols(DP_broad)
-name_cols(DP_strict)
+))
 
 for (i in 1:length(DP_broad$ref)) {
 
-  archlexeme         <- data[i, 1]
-  dispersions_broad  <- DP_broad[i, ]
-  dispersions_strict <- DP_strict[i, ]
-  item_data          <- data.frame(dispersions_broad)
-  item_data          <- rbind(item_data, dispersions_strict)
+  archlexeme <- data[i, 1]
+
+  rel_freqs_broad     <- rel_freq_broad[i, ]
+  rel_freqs_strict    <- rel_freq_strict[i, ]
+  rel_freqs_item_data <- data.frame(rel_freqs_broad)
+  rel_freqs_item_data <- rbind(rel_freqs_item_data, rel_freqs_strict)
+
+  plot_rel_freqs <- ggtern(
+    rel_freqs_item_data,
+    aes(ref, pred, mod)
+  ) +
+    labs(
+      title = archlexeme,
+      subtitle = "Relative Frequencies"
+    ) +
+    theme (
+      plot.title.position = "plot",
+      plot.title          = element_text(hjust = 0.5),
+      plot.subtitle       = element_text(hjust = 0.5),
+      tern.axis.title.R   = element_text(hjust = 1.25),
+      tern.axis.title.L   = element_text(hjust = -0.25)
+    ) +
+    theme_hidelabels() +
+    Tlab("Predication") +
+    Llab("Reference") +
+    Rlab("Modification") +
+    tern_limits(T = 1.1, L = 1.1, R = 1.1) +
+    geom_point() +
+    geom_text(aes(label = c("broad", "strict")), vjust = 1.5)
+
+  dispersions_broad     <- DP_broad[i, ]
+  dispersions_strict    <- DP_strict[i, ]
+  dispersions_item_data <- data.frame(dispersions_broad)
+  dispersions_item_data <- rbind(dispersions_item_data, dispersions_strict)
 
   plot_dispersions <- ggtern(
-    item_data,
+    dispersions_item_data,
     aes(ref, pred, mod)
   ) +
     labs(
@@ -40,26 +77,33 @@ for (i in 1:length(DP_broad$ref)) {
     ) +
     theme(
       plot.title.position = "plot",
-      plot.title = element_text(hjust = 0.5),
-      tern.axis.title.R = element_text(hjust = 1.25),
-      tern.axis.title.L = element_text(hjust = -0.25)
+      plot.title          = element_text(hjust = 0.5),
+      plot.subtitle       = element_text(hjust = 0.5),
+      tern.axis.title.R   = element_text(hjust = 1.25),
+      tern.axis.title.L   = element_text(hjust = -0.25)
     ) +
     theme_hidelabels() +
     Tlab("Predication") +
     Llab("Reference") +
     Rlab("Modification") +
-    tern_limits(T = 1.05, L = 1.05, R = 1.05) +
+    tern_limits(T = 1.1, L = 1.1, R = 1.1) +
     geom_point() +
     geom_text(aes(label = c("broad", "strict")), vjust = 1.5)
 
+  plots <- ggtern::grid.arrange(
+    plot_rel_freqs,
+    plot_dispersions,
+    ncol = 2
+  )
+
   ggsave(
     paste(
-      "stats/figures/English_words/comparison/",
+      "stats/figures/English_words/plots/",
       archlexeme,
       ".png",
       sep = ""
     ),
-    plot_dispersions,
+    plots,
     width  = 10,
     height = 10
   )
--- stats/figures/Nuuchahnulth_ref_vs_pred/scatterplot.R
+++ stats/figures/Nuuchahnulth_ref_vs_pred/scatterplot.R
@@ -4,15 +4,30 @@ source("stats/scripts/load_data.R")
 
 file_path <- "stats/data/Nuuchahnulth_archlexemes.tsv"
 data      <- load_data(file_path)
+cols      <- c("ref", "pred")
 
-functions <- 1 - data.frame(
+rel_freq <- data.frame(
+  data$ref_rel,
+  data$pred_rel
+)
+
+DP <- 1 - data.frame(
   data$dispersion_ref,
   data$dispersion_pred
 )
 
-colnames(functions) <- c("ref", "pred")
+colnames(rel_freq) <- cols
+colnames(DP)       <- cols
 
-ggplot(functions, aes(ref, pred)) +
+plot_rel_freq <- ggplot(rel_freq, aes(ref, pred)) +
+  labs(title = "Relative Frequencies") +
+  theme_minimal() +
+  xlab("reference") +
+  ylab("predication") +
+  geom_point()
+
+plot_DP <- ggplot(DP, aes(ref, pred)) +
+  labs(title = "Deviation of Proportions (DP)") +
   theme_minimal() +
   xlab("reference") +
   ylab("predication") +
@@ -20,8 +35,29 @@ ggplot(functions, aes(ref, pred)) +
   ylim(0, 0.8) +
   geom_point()
 
+plots <- ggtern::grid.arrange(
+  plot_rel_freq,
+  plot_DP,
+  ncol = 2
+)
+
 ggsave(
-  "stats/figures/Nuuchahnulth_ref_vs_pred/scatterplot.png",
+  "stats/figures/Nuuchahnulth_ref_vs_pred/rel_freq.png",
+  plot_rel_freq,
   height = 7.5,
   width  = 7.5
+)
+
+ggsave(
+  "stats/figures/Nuuchahnulth_ref_vs_pred/DP.png",
+  plot_DP,
+  height = 7.5,
+  width  = 7.5
+)
+
+ggsave(
+  "stats/figures/Nuuchahnulth_ref_vs_pred/comparison.png",
+  plots,
+  height = 10,
+  width = 10,
 )
