d83f00302346821a7863bff4e256b9b381a56309
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-11 03:56:19 -0500

    DP vs. DPnorm

--- stats/figures/functions/DP_vs_DPnorm/DP_vs_DPnorm.R
+++ stats/figures/functions/DP_vs_DPnorm/DP_vs_DPnorm.R
@@ -21,6 +21,7 @@ functions_English_DPnorm <- 1 - data.frame(
 
 file_path_Nuuchahnulth <- "stats/data/Nuuchahnulth_archlexemes.tsv"
 data_Nuuchahnulth      <- load_data(file_path_Nuuchahnulth)
+data_Nuuchahnulth      <- data_Nuuchahnulth[which(data_Nuuchahnulth$frequency > 1), ]
 
 # Nuuchahnulth DP
 functions_Nuuchahnulth_DP <- 1 - data.frame(
