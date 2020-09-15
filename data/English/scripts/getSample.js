ba10d1225b1ac261df65819d0bb427cbc3912e01
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-14 22:33:24 -0500

    Revert "remove last word from text"
    This reverts commit a8e4a0ceda1e6301fc3196142a6c4645bc37d013.

--- data/English/scripts/getSample.js
+++ data/English/scripts/getSample.js
@@ -1,3 +1,7 @@
+/* eslint-disable
+  max-statements,
+*/
+
 import fs         from 'fs-extra';
 import path       from 'path';
 import processDir from '../../../scripts/utilities/processDir.js';
@@ -7,23 +11,59 @@ const {
   writeJSON,
 } = fs;
 
-const dataDir            = `data/English/sample`;
-const lastUtteranceIndex = 217;
-const lastWordIndex      = 12;
+const dataDir    = `data/English/sample`;
+const targetSize = 8366;
+let corpusSize   = 0;
+
+const badCharsRegExp = /[^A-Za-z]/u;
+const hasBadChars    = str => badCharsRegExp.test(str);
 
 async function getSample(filePath) {
 
-  if (!filePath.includes(`ENG`)) return;
-
   const text = await readJSON(filePath);
 
-  text.utterances = text.utterances.slice(0, lastUtteranceIndex + 1);
+  let utteranceIndex = 0;
 
-  const lastUtterance = text.utterances[text.utterances.length - 1];
+  for (const u of text.utterances) {
 
-  lastUtterance.words = lastUtterance.words.slice(0, lastWordIndex + 1);
+    if (corpusSize >= targetSize) return;
 
-  await writeJSON(filePath, text, { encoding: `utf8`, spaces: 2 });
+    if (!u.words?.length) return;
+
+    let wordIndex = 0;
+
+    for (const w of u.words) {
+
+      if (corpusSize >= targetSize) return;
+
+      if (hasBadChars(w.transcription)) return;
+
+      corpusSize++;
+
+      if (corpusSize >= targetSize) {
+        u.words = u.words.slice(0, wordIndex + 1);
+        return;
+      }
+
+      wordIndex++;
+
+    }
+
+    utteranceIndex++;
+
+    if (corpusSize >= targetSize) {
+      text.utterances = text.utterances.slice(0, utteranceIndex);
+      return;
+    }
+
+  }
+
+  if (corpusSize >= targetSize) {
+
+    console.log(corpusSize);
+    await writeJSON(filePath, text, { encoding: `utf8`, spaces: 2 });
+
+  }
 
 }
 
