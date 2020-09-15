8d292029f858e4c75d13f35d568feb52882c30ae
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-14 19:25:27 -0500

    distribution (100 words)

--- stats/figures/flexibility/distribution.R
+++ stats/figures/flexibility/distribution.R
@@ -1,251 +1,84 @@
 library(cowplot)
 library(ggplot2)
 
-source("stats/scripts/load_data.R")
+source("stats/scripts/load_100.R")
 
-data_English      <- load_data("stats/data/English_archlexemes.tsv")
-data_Nuuchahnulth <- load_data("stats/data/Nuuchahnulth_archlexemes.tsv")
-data_Nuuchahnulth <- data_Nuuchahnulth[which(data_Nuuchahnulth$frequency > 1 & data_Nuuchahnulth$flexibility != "NaN"), ]
-
-data_English_nonzero      <- data_English[which(data_English$flexibility != 0), ]
-data_Nuuchahnulth_nonzero <- data_Nuuchahnulth[which(data_Nuuchahnulth$flexibility != 0), ]
+data <- load_100()
 
 bin_width <- 0.05 # results in 20 bins
 
-English_hist <- ggplot(data_English, aes(x = flexibility)) +
-  labs(title = "English") +
+histogram <- ggplot(data, aes(x = flexibility)) +
   theme(
-    axis.text.x         = element_blank(),
-    axis.ticks.x        = element_blank(),
-    axis.title.x        = element_blank(),
-    plot.margin         = margin(0.5, 0.5, 0, 0.5, "cm"),
-    plot.title          = element_text(hjust = 0.5),
-    plot.title.position = "plot"
+    axis.text.x  = element_blank(),
+    axis.title.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    plot.margin  = margin(0.5, 0.5, 0, 0.5, "cm"),
   ) +
   geom_histogram(
     binwidth = bin_width,
     color    = "black",
-    fill     = "white",
-    na.rm    = TRUE
+    fill     = "white"
   ) +
   geom_density(
-    aes(y = ..count.. * bin_width),
-    alpha = 0.2,
-    fill  = "red"
+    aes(
+      color = language,
+      fill  = language,
+      y     = ..count.. * bin_width,
+    ),
+    alpha       = 0.5,
+    show.legend = FALSE
   ) +
   geom_vline(
-    aes(xintercept = mean(flexibility)),
-    color    = "red",
-    linetype = "dashed",
-    size     = 1
+    aes(
+      color = language,
+      xintercept = mean(flexibility)
+    ),
+    linetype    = "dashed",
+    size        = 1,
+    show.legend = FALSE
   ) +
-  xlim(-0.1, 1)
+  xlim(-0.1, 1) +
+  facet_grid(cols = vars(language))
 
-English_boxplot <- ggplot(data_English, aes(x = flexibility)) +
+boxplot <- ggplot(data, aes(x = flexibility)) +
   theme(
     axis.text.y        = element_blank(),
     axis.ticks.y       = element_blank(),
-    axis.title.y       = element_blank(),
     panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank(),
-    plot.margin        = margin(0, 0.5, 0.5, 0.5, "cm")
+    plot.margin        = margin(0, 0.5, 0.5, 0.5, "cm"),
+    strip.text.x       = element_blank()
   ) +
   geom_boxplot(
-    na.rm = TRUE,
     notch = TRUE,
     width = 0.5
   ) +
   geom_vline(
-    aes(xintercept = mean(flexibility)),
-    color    = "red",
-    linetype = "dashed",
-    size = 1
+    aes(
+      color = language,
+      xintercept = mean(flexibility)
+    ),
+    linetype    = "dashed",
+    show.legend = FALSE,
+    size        = 1
   ) +
   geom_rug(length = unit(0.2, "cm")) +
   xlim(-0.1, 1) +
-  ylim(-0.5, 0.5)
-
-Nuuchahnulth_hist <- ggplot(data_Nuuchahnulth, aes(x = flexibility)) +
-  labs(title = "Nuuchahnulth") +
-  theme(
-    axis.text.x         = element_blank(),
-    axis.ticks.x        = element_blank(),
-    axis.title.x        = element_blank(),
-    plot.margin         = margin(0.5, 0.5, 0, 0.5, "cm"),
-    plot.title          = element_text(hjust = 0.5),
-    plot.title.position = "plot"
-  ) +
-  geom_histogram(
-    binwidth = bin_width,
-    color    = "black",
-    fill     = "white",
-    na.rm    = TRUE
-  ) +
-  geom_density(
-    aes(y = ..count.. * bin_width),
-    alpha = 0.2,
-    fill  = "blue"
-  ) +
-  geom_vline(
-    aes(xintercept = mean(flexibility)),
-    color    = "blue",
-    linetype = "dashed",
-    size     = 1
-  ) +
-  xlim(-0.1, 1)
-
-Nuuchahnulth_boxplot <- ggplot(data_Nuuchahnulth, aes(x = flexibility)) +
-  theme(
-    axis.text.y        = element_blank(),
-    axis.ticks.y       = element_blank(),
-    axis.title.y       = element_blank(),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    plot.margin        = margin(0, 0.5, 0.5, 0.5, "cm")
-  ) +
-  geom_boxplot(
-    na.rm = TRUE,
-    notch = TRUE,
-    width = 0.5
-  ) +
-  geom_vline(
-    aes(xintercept = mean(flexibility)),
-    color    = "blue",
-    linetype = "dashed",
-    size = 1
-  ) +
-  geom_rug(length = unit(0.2, "cm")) +
-  xlim(-0.1, 1) +
-  ylim(-0.5, 0.5)
-
-English_hist_nonzero <- ggplot(data_English_nonzero, aes(x = flexibility)) +
-  labs(title = "English (nonzero)") +
-  theme(
-    axis.text.x         = element_blank(),
-    axis.ticks.x        = element_blank(),
-    axis.title.x        = element_blank(),
-    plot.margin         = margin(0.5, 0.5, 0, 0.5, "cm"),
-    plot.title          = element_text(hjust = 0.5),
-    plot.title.position = "plot"
-  ) +
-  geom_histogram(
-    binwidth = bin_width,
-    color    = "black",
-    fill     = "white",
-    na.rm    = TRUE
-  ) +
-  geom_density(
-    aes(y = ..count.. * bin_width),
-    alpha = 0.2,
-    fill  = "red"
-  ) +
-  geom_vline(
-    aes(xintercept = mean(flexibility)),
-    color    = "red",
-    linetype = "dashed",
-    size     = 1
-  ) +
-  xlim(-0.1, 1) +
-  ylim(0, 45)
-
-English_boxplot_nonzero <- ggplot(data_English_nonzero, aes(x = flexibility)) +
-  theme(
-    axis.text.y        = element_blank(),
-    axis.ticks.y       = element_blank(),
-    axis.title.y       = element_blank(),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    plot.margin        = margin(0, 0.5, 0.5, 0.5, "cm")
-  ) +
-  geom_boxplot(
-    na.rm = TRUE,
-    notch = TRUE,
-    width = 0.5
-  ) +
-  geom_vline(
-    aes(xintercept = mean(flexibility)),
-    color    = "red",
-    linetype = "dashed",
-    size = 1
-  ) +
-  geom_rug(length = unit(0.2, "cm")) +
-  xlim(-0.1, 1) +
-  ylim(-0.5, 0.5)
-
-Nuuchahnulth_hist_nonzero <- ggplot(data_Nuuchahnulth_nonzero, aes(x = flexibility)) +
-  labs(title = "Nuuchahnulth (nonzero)") +
-  theme(
-    axis.text.x         = element_blank(),
-    axis.ticks.x        = element_blank(),
-    axis.title.x        = element_blank(),
-    plot.margin         = margin(0.5, 0.5, 0, 0.5, "cm"),
-    plot.title          = element_text(hjust = 0.5),
-    plot.title.position = "plot"
-  ) +
-  geom_histogram(
-    binwidth = bin_width,
-    color    = "black",
-    fill     = "white",
-    na.rm    = TRUE
-  ) +
-  geom_density(
-    aes(y = ..count.. * bin_width),
-    alpha = 0.2,
-    fill  = "blue"
-  ) +
-  geom_vline(
-    aes(xintercept = mean(flexibility)),
-    color    = "blue",
-    linetype = "dashed",
-    size     = 1
-  ) +
-  xlim(-0.1, 1) +
-  ylim(0, 45)
-
-Nuuchahnulth_boxplot_nonzero <- ggplot(data_Nuuchahnulth_nonzero, aes(x = flexibility)) +
-  theme(
-    axis.text.y        = element_blank(),
-    axis.ticks.y       = element_blank(),
-    axis.title.y       = element_blank(),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    plot.margin        = margin(0, 0.5, 0.5, 0.5, "cm")
-  ) +
-  geom_boxplot(
-    na.rm = TRUE,
-    notch = TRUE,
-    width = 0.5
-  ) +
-  geom_vline(
-    aes(xintercept = mean(flexibility)),
-    color    = "blue",
-    linetype = "dashed",
-    size = 1
-  ) +
-  geom_rug(length = unit(0.2, "cm")) +
-  xlim(-0.1, 1) +
-  ylim(-0.5, 0.5)
+  ylim(-0.5, 0.5) +
+  facet_grid(cols = vars(language))
 
 grid <- plot_grid(
-  English_hist,
-  Nuuchahnulth_hist,
-  English_boxplot,
-  Nuuchahnulth_boxplot,
-  English_hist_nonzero,
-  Nuuchahnulth_hist_nonzero,
-  English_boxplot_nonzero,
-  Nuuchahnulth_boxplot_nonzero,
+  histogram,
+  boxplot,
   align = "v",
-  ncol  = 2,
-  nrow  = 4,
-  rel_heights = c(3.5, 1, 3.5, 1)
+  ncol  = 1,
+  nrow  = 2,
+  rel_heights = c(3.5, 1)
 )
 
 grid
 
 ggsave(
   "stats/figures/flexibility/distribution.png",
-  grid,
-  height = 10,
-  width  = 10
+  grid
 )
