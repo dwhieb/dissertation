b0a050f64f622791ea3fbdbd5b6d9597c5d858f5
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-14 22:51:58 -0500

    clear unnecessary tags from sample

--- data/English/scripts/clearTags.js
+++ data/English/scripts/clearTags.js
@@ -7,7 +7,7 @@ const {
   writeJSON,
 } = fs;
 
-const dataDir = 'data/english/texts';
+const dataDir = 'data/English/sample';
 
 /**
  * Ignore method which tells recursive-readdir to ignore any non-JSON files
@@ -17,14 +17,21 @@ function ignore(filePath, stats) {
   return path.extname(filePath) !== `.json`;
 }
 
-async function updateTranscriptions(filePath) {
+async function clearTags(filePath) {
 
   const text = await readJSON(filePath);
 
-  text.type = 'Text';
+  text.utterances.forEach(u => {
+
+    u.words.forEach(w => {
+      const { function: func } = w.tags;
+      w.tags = { function: func };
+    });
+
+  });
 
   await writeJSON(filePath, text, { encoding: `utf8`, spaces: 2 });
 
 }
 
-processDir(dataDir, updateTranscriptions, ignore);
+processDir(dataDir, clearTags, ignore);
