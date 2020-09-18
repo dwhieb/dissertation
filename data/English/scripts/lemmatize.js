cfcad014fed47e058e6e50b3d7d578a690b463b6
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-17 21:53:41 -0500

    English: lemmatize.js

---
+++ data/English/scripts/lemmatize.js
@@ -0,0 +1,66 @@
+/**
+ * This script lemmatizes the English corpus using the latest set of wordform > lexeme mappings
+ */
+
+/* eslint-disable
+  max-nested-callbacks,
+*/
+
+import fs         from 'fs';
+import path       from 'path';
+import processDir from '../../../scripts/utilities/processDir.js';
+
+const { readJSON, writeJSON } = fs;
+
+const dataDir       = `data/English/texts`;
+const wordformsPath = `data/English/subcorpus_wordforms.json`;
+
+const whitelist = [
+  `GER`,
+  `INF`,
+  `PREDCXN`,
+  `MOD`,
+  `PRED`,
+  `REF`,
+];
+
+function ignore(filePath, stats) {
+  if (stats.isDirectory()) return false;
+  return path.extname(filePath) !== `.json`;
+}
+
+void async function lemmatizeTexts() {
+
+  const wordforms = await readJSON(wordformsPath);
+
+  await processDir(dataDir, async filePath => {
+
+    const text = await readJSON(filePath);
+
+    text.utterances.forEach(u => {
+
+      if (!u.words?.length) return;
+
+      u.words.forEach(w => {
+
+        const { function: pragmaticFunction } = w.tags;
+
+        if (!pragmaticFunction) return;
+        if (!whitelist.includes(pragmaticFunction)) return;
+
+        const wordform = w.transcription.default;
+        const lexeme   = wordforms[wordform];
+
+        if (!lexeme) return;
+
+        w.stem = w.stem ?? lexeme; // eslint-disable-line no-param-reassign
+
+      });
+
+    });
+
+    await writeJSON(filePath, text, { encoding: `utf8`, spaces: 2 });
+
+  }, ignore);
+
+}();
