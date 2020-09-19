746abe22b9df783cb13ddcae89a5b956efe622e1
Daniel W. Hieber <dwhieb@gmail.com>
2020-09-19 18:29:17 -0500

    Nuuchahnulth root functions

--- stats/figures/functions/English_vs_Nuuchahnulth/small.R
+++ stats/figures/functions/English_vs_Nuuchahnulth/small.R
@@ -4,9 +4,9 @@ source("stats/scripts/load_small.R")
 
 data <- load_small()
 
-data$dispersion_ref  = 1 - data$dispersion_ref
-data$dispersion_pred = 1 - data$dispersion_pred
-data$dispersion_mod  = 1 - data$dispersion_mod
+data$dispersion_ref  <- 1 - data$dispersion_ref
+data$dispersion_pred <- 1 - data$dispersion_pred
+data$dispersion_mod  <- 1 - data$dispersion_mod
 
 plot <- ggtern(data, aes(
   dispersion_ref,
@@ -20,9 +20,9 @@ plot <- ggtern(data, aes(
     tern.axis.title.L = element_text(hjust = -0.25),
     tern.axis.title.R = element_text(hjust = 1.25)
   ) +
-  Tlab("Predication") +
-  Llab("Reference") +
-  Rlab("Modification") +
+  Tlab("predication") +
+  Llab("reference") +
+  Rlab("modification") +
   tern_limits(T = 1.05, L = 1.05, R = 1.05) +
   geom_polygon(
     data = data.frame(
