ac02822061e461f4d617a547dab4abb8626aefb1
Daniel W. Hieber <dwhieb@gmail.com>
2020-11-11 08:12:43 -0600

    update definiteness script

--- stats/figures/definiteness/plot.R
+++ stats/figures/definiteness/plot.R
@@ -1,5 +1,9 @@
+# This script was started to plot the interaction of definite markers
+# and aspect, but it turns out there are hardly any cases where a stem
+# appears with both an aspect marker and a definite marker.
+
 source("stats/scripts/load_data.R")
 
-data <- load_data("stats/data/Nuuchahnulth.tsv")
+data <- load_data("stats/data/Nuuchahnulth_stems.tsv")
 data <- data[which(data$definite > 0), ]
 data <- data[which(!is.na(data$aspect)), ]
