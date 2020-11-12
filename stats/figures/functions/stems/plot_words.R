8243c2b532cd2a5881290f5a9b2c6b16fa2f0418
Daniel W. Hieber <dwhieb@gmail.com>
2020-11-11 18:07:07 -0600

    plot functions of individual words

--- stats/figures/functions/stems/plot_words.R
+++ stats/figures/functions/stems/plot_words.R
@@ -4,7 +4,7 @@ source("stats/figures/functions/stems/plot_word.R")
 library(ggtern)
 
 # NB: Path should end in a slash
-plot_words <- function(path, data) {
+plot_words <- function(path, data, lang) {
 
   rel_freq <- data.frame(
     data$ref_rel,
@@ -19,10 +19,19 @@ plot_words <- function(path, data) {
   )
 
   for (i in 1:length(data$item)) {
-
+    
     item       <- data[i, 1]
     gloss      <- data[i, 2]
-    item_label <- paste(item, gloss, sep = " ")
+    file_label <- "unknown"
+    item_label <- "unknown"
+    
+    if (lang == "English") {
+      file_label <- item
+      item_label <- item
+    } else {
+      file_label <- gloss
+      item_label <- paste(item, gloss, sep = " ")
+    }
 
     plot_rel_freq   <- plot_word(rel_freq[i, ], item_label, "Relative Frequencies")
     plot_dispersion <- plot_word(DP[i, ], item_label, "Deviation of Proportions (DP)")
@@ -34,7 +43,7 @@ plot_words <- function(path, data) {
     )
 
     ggsave(
-      paste(path, gloss, ".png", sep = ""),
+      paste(path, file_label, ".png", sep = ""),
       plots,
       height = 5,
       width  = 10
@@ -45,13 +54,13 @@ plot_words <- function(path, data) {
 }
 
 data_Nuu <- load_data("stats/data/Nuuchahnulth_stems.tsv")
-data_Nuu <- data[which(data$frequency >= 4), ]
-data_Nuu <- data[order(-data$flexibility), ]
-data_Nuu <- data[1:100, ]
+data_Nuu <- data_Nuu[which(data_Nuu$frequency >= 4), ]
+data_Nuu <- data_Nuu[order(-data_Nuu$flexibility), ]
+data_Nuu <- data_Nuu[1:100, ]
 
 data_Eng <- load_data("stats/data/English_stems.tsv")
-data_Eng <- data[which(data$frequency >= 4), ]
-data_Eng <- data[order(-data$flexibility), ]
+data_Eng <- data_Eng[which(data_Eng$frequency >= 4), ]
+data_Eng <- data_Eng[order(-data_Eng$flexibility), ]
 
-plot_words("stats/figures/functions/stems/Nuuchahnulth/", data_Nuu)
-plot_words("stats/figures/functions/stems/English", data_Eng)
+plot_words("stats/figures/functions/stems/Nuuchahnulth/", data_Nuu, "Nuuchahnulth")
+plot_words("stats/figures/functions/stems/English/", data_Eng, "English")
