f6455cfa175b4d5cb869b781f7bf822ca461a043
Daniel W. Hieber <dwhieb@gmail.com>
2021-01-23 13:25:04 -0700

    Section 4.4 in progress

--- stats/figures/corpus_size/corpus_size.R
+++ stats/figures/corpus_size/corpus_size.R
@@ -51,6 +51,7 @@ plot_Nuu <- ggplot(data_Nuu, aes(
     plot.title          = element_text(hjust = 0.5),
     plot.title.position = "plot"
   ) +
+  ylim(0, 1) +
   geom_point(
     show.legend = FALSE,
     size        = 0.5
@@ -64,8 +65,8 @@ ggsave(
 )
 
 plot_Eng <- ggplot(data_Eng, aes(
-  x     = tokens_observed,
-  y     = flexibility
+  x = tokens_observed,
+  y = flexibility
 )) +
   labs(title = "English") +
   ylab("flexibility") +
@@ -75,6 +76,7 @@ plot_Eng <- ggplot(data_Eng, aes(
     plot.title          = element_text(hjust = 0.5),
     plot.title.position = "plot"
   ) +
+  ylim(0, 1) +
   geom_point(
     show.legend = FALSE,
     size        = 0.5
@@ -86,4 +88,87 @@ ggsave(
   plot_Eng,
   height = 10,
   width  = 20
+)
+
+high_frequency_Eng_words <- c(
+  "know",
+  "think",
+  "one",
+  "thing",
+  "right",
+  "say",
+  "see",
+  "time",
+  "good",
+  "year"
+)
+
+data_Eng_words <- data_Eng[data_Eng$stem %in% high_frequency_Eng_words, ]
+
+plot_Eng_words <- ggplot(data_Eng_words, aes(
+  x = tokens_observed,
+  y = flexibility,
+  color = stem
+)) +
+  labs(title = "high-frequency English stems") +
+  ylab("flexibility") +
+  xlab("# tokens observed") +
+  theme_minimal() +
+  theme(
+    plot.title          = element_text(hjust = 0.5),
+    plot.title.position = "plot"
+  ) +
+  ylim(0, 1) +
+  geom_point(
+    show.legend = TRUE,
+    size        = 1
+  ) # +
+  # scale_x_continuous(trans="log10")
+
+ggsave(
+  "stats/figures/corpus_size/English_high_freq.png",
+  plot_Eng_words,
+  height = 5,
+  width  = 10
+)
+
+high_frequency_Nuu_words <- c(
+  "wa·",
+  "wik",
+  "waː",
+  "qʷaː",
+  "ʔaḥʔaː",
+  "ʔaḥ",
+  "čuː",
+  "quːʔas",
+  "qʷayac̓iːk",
+  "qʷis"
+)
+
+data_Nuu_words <- data_Nuu[data_Nuu$stem %in% high_frequency_Nuu_words, ]
+
+plot_Nuu_words <- ggplot(data_Nuu_words, aes(
+  x = tokens_observed,
+  y = flexibility,
+  color = stem
+)) +
+  labs(title = "high-frequency Nuuchahnulth stems") +
+  ylab("flexibility") +
+  xlab("# tokens observed") +
+  theme_minimal() +
+  theme(
+    plot.title          = element_text(hjust = 0.5),
+    plot.title.position = "plot"
+  ) +
+  ylim(0, 1) +
+  geom_point(
+    show.legend = TRUE,
+    size        = 1
+  )
+
+ggsave(
+  "stats/figures/corpus_size/Nuuchahnulth_high_freq.png",
+  plot_Nuu_words,
+  height = 5,
+  width  = 10
 )
