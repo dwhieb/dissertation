539a41f0e24f1cd6e7cc2aac1067008c7df7dd37
Daniel W. Hieber <dwhieb@gmail.com>
2021-02-25 14:38:01 -0600

    use log relative frequency for all GAMs

--- stats/figures/freq-DP_vs_flexibility/models.R
+++ stats/figures/freq-DP_vs_flexibility/models.R
@@ -27,10 +27,12 @@ load_data <- function(file_path) {
 # DATA
 
 # English small corpus sample
-data_Eng_small <- load_data("stats/figures/freq-DP_vs_flexibility/Eng_small.csv")
+data_Eng_small              <- load_data("stats/figures/freq-DP_vs_flexibility/Eng_small.csv")
+data_Eng_small$log_rel_freq <- log2(data_Eng_small$rel_freq)
 
 # Nuuchahnulth small corpus sample
-data_Nuu_small <- load_data("stats/figures/freq-DP_vs_flexibility/Nuu_small.csv")
+data_Nuu_small              <- load_data("stats/figures/freq-DP_vs_flexibility/Nuu_small.csv")
+data_Nuu_small$log_rel_freq <- log2(data_Nuu_small$rel_freq)
 
 # English 100-item sample
 data_Eng_100              <- load_data("stats/figures/freq-DP_vs_flexibility/Eng_100.csv")
@@ -44,7 +46,7 @@ data_Nuu_100$log_rel_freq <- log2(data_Nuu_100$rel_freq)
 
 # English small corpus sample
 model_Eng_small <- gam(
-  flexibility ~ s(rel_freq) + s(dispersion) + ti(rel_freq, dispersion),
+  flexibility ~ s(log_rel_freq) + s(dispersion) + ti(log_rel_freq, dispersion),
   data   = data_Eng_small,
   method = "REML",
 )
@@ -53,7 +55,7 @@ summary(model_Eng_small)
 
 # Nuuchahnulth small corpus sample
 model_Nuu_small <- gam(
-  flexibility ~ s(rel_freq) + s(dispersion) + ti(rel_freq, dispersion),
+  flexibility ~ s(log_rel_freq) + s(dispersion) + ti(log_rel_freq, dispersion),
   data   = data_Nuu_100,
   method = "REML",
 )
--- stats/figures/freq-DP_vs_flexibility/models_STG.R
+++ stats/figures/freq-DP_vs_flexibility/models_STG.R
@@ -27,10 +27,12 @@ load_data <- function(file_path) {
 # DATA
 
 # English small corpus sample
-data_Eng_small <- load_data("Eng_small.csv")
+data_Eng_small              <- load_data("Eng_small.csv")
+data_Eng_small$log_rel_freq <- log2(data_Eng_small$rel_freq)
 
 # Nuuchahnulth small corpus sample
-data_Nuu_small <- load_data("Nuu_small.csv")
+data_Nuu_small              <- load_data("Nuu_small.csv")
+data_Nuu_small$log_rel_freq <- log2(data_Nuu_small$rel_freq)
 
 # English 100-item sample
 data_Eng_100              <- load_data("Eng_100.csv")
@@ -44,7 +46,7 @@ data_Nuu_100$log_rel_freq <- log2(data_Nuu_100$rel_freq)
 
 # English small corpus sample
 model_Eng_small <- gam(
-  flexibility ~ s(rel_freq) + s(dispersion) + ti(rel_freq, dispersion),
+  flexibility ~ s(log_rel_freq) + s(dispersion) + ti(log_rel_freq, dispersion),
   data   = data_Eng_small,
   method = "REML",
 )
@@ -53,7 +55,7 @@ summary(model_Eng_small)
 
 # Nuuchahnulth small corpus sample
 model_Nuu_small <- gam(
-  flexibility ~ s(rel_freq) + s(dispersion) + ti(rel_freq, dispersion),
+  flexibility ~ s(log_rel_freq) + s(dispersion) + ti(log_rel_freq, dispersion),
   data   = data_Nuu_100,
   method = "REML",
 )
