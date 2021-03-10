8aa554fb8aed62e20b37a8ba3ff844403ffbee00
Daniel W. Hieber <dwhieb@gmail.com>
2021-03-10 12:17:43 -0600

    update bin script for getCumulativeFlexibility.js

--- stats/scripts/bin/getCumulativeFlexibility.js
+++ stats/scripts/bin/getCumulativeFlexibility.js
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
 
-import getCumulativeFlexibility from '../getCumulativeFlexibility.js';
+/**
+ * Calculate cumulative flexibility for a corpus.
+ *
+ * Arguments:
+ *
+ * <corpus> (required)                       "English" or "Nuuchahnulth"
+ * -o, --out (optional; default = "out.tsv") The path where you would like the data output
+ * -s, --samples (optional; default = 1)     The number of samples to run
+ */
 
-const [,, language, outputPath] = process.argv;
+import getCumulativeFlexibility from '../getCumulativeFlexibility.js';
+import parseArgs                from 'yargs-parser';
+
+const {
+  _: [language],
+  o,
+  out,
+  s,
+  samples,
+} = parseArgs(process.argv.slice(2));
 
 if (!language) {
   throw new Error(`Please provide the language as the first argument: English | Nuuchahnulth`);
 }
 
-getCumulativeFlexibility(language, outputPath)
+getCumulativeFlexibility(
+  language,
+  o || out,
+  s || samples,
+)
 .catch(e => { throw e; });
