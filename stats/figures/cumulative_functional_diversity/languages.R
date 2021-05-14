e0694ffca9fd9a2e21bf08fab24cc5f64e6cc105
Daniel W. Hieber <dwhieb@gmail.com>
2021-05-14 10:50:32 -0500

    cumulative functional diversity by language

--- stats/figures/cumulative_functional_diversity/languages.R
+++ stats/figures/cumulative_functional_diversity/languages.R
@@ -2,18 +2,18 @@ library(ggplot2)
 
 display_points <- 100000
 
-data_Eng            <- read.table("stats/cumulative_flexibility/English.tsv")
-colnames(data_Eng)  <- c("sample", "cumulative_flexibility")
+data_Eng            <- read.table("stats/cumulative_functional_diversity/English.tsv")
+colnames(data_Eng)  <- c("sample", "cumulative_functional_diversity")
 numObservations_Eng <- nrow(data_Eng)
 data_Eng$token      <- sequence(rle(as.character(data_Eng$sample))$lengths)
 data_Eng_subset     <- data_Eng[seq(1, numObservations_Eng, numObservations_Eng / display_points), ]
 
 plot_Eng <- ggplot(data_Eng_subset, aes(
     x = token,
-    y = cumulative_flexibility
+    y = cumulative_functional_diversity
   )) +
-  labs(title = "cumulative flexibility (English)") +
-  ylab("cumulative flexibility (Shannon's H)") +
+  labs(title = "cumulative functional diversity (English)") +
+  ylab("cumulative functional diversity (Shannon's H)") +
   xlab("# tokens observed") +
   theme_minimal() +
   theme(
@@ -26,24 +26,24 @@ plot_Eng <- ggplot(data_Eng_subset, aes(
 plot_Eng
 
 ggsave(
-  "stats/figures/cumulative_flexibility/English.png",
+  "stats/figures/cumulative_functional_diversity/English.png",
   plot_Eng,
   height = 5,
   width  = 10
 )
 
-data_Nuu            <- read.table("stats/cumulative_flexibility/Nuuchahnulth.tsv")
-colnames(data_Nuu)  <- c("sample", "cumulative_flexibility")
+data_Nuu            <- read.table("stats/cumulative_functional_diversity/Nuuchahnulth.tsv")
+colnames(data_Nuu)  <- c("sample", "cumulative_functional_diversity")
 numObservations_Nuu <- nrow(data_Nuu)
 data_Nuu$token      <- sequence(rle(as.character(data_Nuu$sample))$lengths)
 data_Nuu_subset     <- data_Nuu[seq(1, numObservations_Nuu, numObservations_Nuu / display_points), ]
 
 plot_Nuu <- ggplot(data_Nuu_subset, aes(
     x = token,
-    y = cumulative_flexibility
+    y = cumulative_functional_diversity
   )) +
-  labs(title = "cumulative flexibility (Nuuchahnulth)") +
-  ylab("cumulative flexibility (Shannon's H)") +
+  labs(title = "cumulative functional diversity (Nuuchahnulth)") +
+  ylab("cumulative functional diversity (Shannon's H)") +
   xlab("# tokens observed") +
   theme_minimal() +
   theme(
@@ -56,7 +56,7 @@ plot_Nuu <- ggplot(data_Nuu_subset, aes(
 plot_Nuu
 
 ggsave(
-  "stats/figures/cumulative_flexibility/Nuuchahnulth.png",
+  "stats/figures/cumulative_functional_diversity/Nuuchahnulth.png",
   plot_Nuu,
   height = 5,
   width  = 10
